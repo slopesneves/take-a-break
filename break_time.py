5e19ae08ad64d7108eeb62061815336a16b67b02
Stephane LOPES-NEVES <stephane@lopes-neves.fr>
2016-05-14 14:22:56 +0200

    Execute the break time 3 times

--- break_time.py
+++ break_time.py
@@ -1,5 +1,8 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=DgwzRB2ijHY")
+count = 0
+while (count < 3):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=DgwzRB2ijHY")
+    count = count + 1
