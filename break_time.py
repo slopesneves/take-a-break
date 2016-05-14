1c5d33ce77b00540b8ecda3b671acf01756997da
Stephane LOPES-NEVES <stephane@lopes-neves.fr>
2016-05-14 14:29:15 +0200

    Adding message when the program start

--- break_time.py
+++ break_time.py
@@ -2,6 +2,7 @@ import time
 import webbrowser
 
 count = 0
+print("This program started at ", time.ctime())
 while (count < 3):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=DgwzRB2ijHY")
